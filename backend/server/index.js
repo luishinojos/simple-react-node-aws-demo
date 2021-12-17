ace75f3020dafd6001e66c96d22663c73342d860
luishinojos <luis.hinojos@wizeline.com>
2021-12-17 00:40:36 -0700

    [add] aws sdk presigned url

--- backend/server/index.js
+++ backend/server/index.js
@@ -6,6 +6,33 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the Region 
+AWS.config.update({ region: 'us-west-1' });
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+
+app.get("/test", (req, res) => {
+    s3.listBuckets(function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data.Buckets);
+            res.json({ datda: data.Buckets });
+        }
+    });
+});
+
+app.get("/signed", (req, res) => {
+    var params = { Bucket: 'luishinojos-demobucket', Key: 'it-worked.gif', Expires: 60 };
+    var url = s3.getSignedUrl('getObject', params);
+    console.log('The URL is', url); // expires in 60 seconds
+    res.json({ url: url });
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@
 
 import React from "react";
 import logo from "./logo.svg";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [URL, setURL] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api")
@@ -13,11 +15,17 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  const handleGetImage = async () => {
+    const data = await axios.get("/signed")
+    setURL(data.data.url)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <button onClick={handleGetImage}>Fetch image from aws</button>
+        <img src={URL} alt="gif" />
       </header>
     </div>
   );
